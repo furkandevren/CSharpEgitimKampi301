6705d6e9dc6629b8ca85d56d3be217fad45c5bda
furkandevren47@gmail.com <furkandevren47@gmail.com>
2024-11-30 00:29:55 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EFProject/FrmStatistics.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.EFProject
+{
+    public partial class FrmStatistics : Form
+    {
+        public FrmStatistics()
+        {
+            InitializeComponent();
+        }
+        EgitimKampiEFTravelDbEntities2 db = new EgitimKampiEFTravelDbEntities2();
+        private void FrmStatistics_Load(object sender, EventArgs e)
+        {
+
+            lblLocationCount.Text = db.Location.Count().ToString();
+            lblSumCapacity.Text = db.Location.Sum(x => x.Capacity).ToString();
+            lblGuideCount.Text = db.Guide.Count().ToString();
+            lblAvgCapacity.Text = db.Location.Average(x => x.Capacity).ToString();
+            lblAvgLocationPrice.Text = db.Location.Average(x => x.Price).ToString() + " ₺";
+
+            int lastCountryId = db.Location.Max(x => x.LocationId);
+            lblLastCountryName.Text = db.Location.Where(x => x.LocationId == lastCountryId).Select(y => y.Country).FirstOrDefault();
+
+            lblCappadociaLocationCapacity.Text = db.Location.Where(x => x.City == "Kapadokya").Select(y => y.Capacity).FirstOrDefault().ToString();
+
+            lblTurkiyeCapacityAvg.Text = db.Location.Where(x => x.Country == "Türkiye").Average(y => y.Capacity).ToString();
+
+            var romeGuideId = db.Location.Where(x => x.City == "Roma").Select(y => y.GuideId).FirstOrDefault();
+            lblRomeGuidName.Text = db.Guide.Where(x => x.GuideId == romeGuideId).Select(y => y.GuideName + " " + y.GuideSurname).FirstOrDefault();
+
+            var maxCapacity = db.Location.Max(x => x.Capacity);
+            lblMaxCapacityLocation.Text = db.Location.Where(x => x.Capacity == maxCapacity).Select(y => y.City).FirstOrDefault().ToString();
+
+            var maxPrice = db.Location.Max(x => x.Price);
+            lblMaxPriceLocation.Text=db.Location.Where(x => x.Price == maxPrice).Select(y => y.City).FirstOrDefault().ToString();
+
+            var guideIdByNameFurkanDevren=db.Guide.Where(x=>x.GuideName=="Furkan"&& x.GuideSurname=="Devren").Select(y=>y.GuideId).FirstOrDefault();
+            lblFurkanDevrenLocationCount.Text = db.Location.Where(x => x.GuideId == guideIdByNameFurkanDevren).Count().ToString();
+        }
+
+    }
+}
---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        //int x; //Field
+        //public int y { get; set; }//property
+
+        //void test()
+        //{
+        //    int z; // variable
+        //}
+
+        public int CategoryId { get; set; } //CodeFirst yaklaşımında bunun birincil anahtar olduğunu tanımlamak için oluşturacağımız prop Clasın adıyla aynı olamlı.
+        public int CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
+
+    }
+}
+/*
+ Field-Variable-Property
+ */
+
+/*
+ int x; --> Field
+ */
